fd7e4f6738538aaec7c594c905155c28c5aea541
nikhilpn725 <nikhilpn725@gmail.com>
2023-05-22 22:51:57 +0530

    mobile padding fixed for title

---
+++ components/Title.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Title = () => {
+    return (
+        <div className="space-y-1  ">
+            <h2 className="z-10 text-3xl font-bold text-center text-transparent duration-1000 bg-white cursor-default text-stroke animate-title sm:text-5xl md:text-6xl whitespace-nowrap bg-clip-text">
+                Join the waitlist for
+            </h2>
+            <h1 className="z-10 text-2xl font-bold text-center text-transparent duration-1000 bg-white cursor-default sm:text-6xl md:text-7xl whitespace-nowrap bg-clip-text bg-gradient-to-r from-purple-300 to-purple-800 animate-fade-in-3">
+                H3lios Blockchain Launch
+            </h1>
+        </div>
+    )
+}
+
+export default Title
